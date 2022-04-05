e648e7d8689880173df668f7720962bdbd34bfd6
entonibaba0720 <entonibaba0720@gmail.com>
2022-04-05 20:31:56 +0200

    database setup

--- config/server.js
+++ config/server.js
@@ -1,7 +1,26 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
+    },
+
+  //postgres://gvuhdkohpughcu:14efec71fcd2089e08e1cdfd7a329ae22b46341acfcdcc0eb69abedd774e901a@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/dd53ombp4p64p6
+
 });
